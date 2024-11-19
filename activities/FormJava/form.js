f74f975dd20dfbf2bd4765093b85b5cf96192c65
Markcastillo-dev <markvincentcastillo143@gmail.com>
2024-11-18 20:30:51 -1000

    Save

--- activities/FormJava/form.js
+++ activities/FormJava/form.js
@@ -3,39 +3,38 @@ document.getElementById("myForm").addEventListener('submit',function(event) {
     event.preventDefault();
     const fname =document.getElementById('fname').value;
     const lname =document.getElementById('lname').value;
-    const age = document.getElementById(age).value;
+    const phone = document.getElementById('phone').value;
     
     
     
     const formData = {
         fname: fname,
         lname: lname,
-        password: document.getElementById('password').value,
-        state: document.getElementById('state').value
+        password: document.getElementById('pass').value,
+        phone: phone
     }
-    if (fname || lname) {
+    if (!fname || !lname) {
         alert('First name and Last name required');
         return;
     };
     
     {
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "submit.json", true);
-    xhr.setRequestHeader("Content-Type,", "application/json;charset=UTF-8");
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Content-Type","application/json;charset=UTF-8");
     xhr.onreadystatechange = function () {
         if (xdr.readtState === 4 && xhr.status ===200) {
-            message = JSON.parse(xhr.response)
+            message = JSON.parse(xhr.response);
             document.getElementById("message").innerHTML = response.message;
             document.getElementById("myForm").innerHTML = "";
-
+            alert("You submmitted the forms")
         }else if (xhr.readyState === 4) {
             alert("Error Submitting form.");
         }
         }
 
     };
-    xhr.send(JSON.stringify(formData))
-
+    xhr.send(JSON.stringify(formData));
     console.log(formData);
-    alert("You submmitted the forms")
+    
 });
